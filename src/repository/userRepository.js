2a6a3a585b2e36ef082774d2664ada0d1870268b
DESKTOP-CGK9SIM\bruno <bruno.braga@localize.com.br>
2022-07-21 22:56:59 -0300

    fix: correcao login

--- src/repository/userRepository.js
+++ src/repository/userRepository.js
@@ -124,7 +124,7 @@ const createUser = async (username, email, password, typeUser) => {
                 '${password}', 
                  true, 
                 '${typeUser}')
-            RETURNING cod_usuario
+            returning cod_usuario        
         `);
     }catch(error){
         throw error;
